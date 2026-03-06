85c87a745195b093f88758fb859a4752e348e90d
Karol <karol590m@gmail.com>
2026-03-06 19:39:27 +0000

    Fix for buffer overflow

--- applications/msg_queues/src/consumer.c
+++ applications/msg_queues/src/consumer.c
@@ -4,12 +4,17 @@
 LOG_MODULE_REGISTER(consumer, LOG_LEVEL_INF);
 
 /* Setting the thread for Consumer work*/
-#define CONSUMER_STACK_SIZE 500
+#define CONSUMER_STACK_SIZE 2048
 #define CONSUMER_PRIORITY 5
+#define MAX_CITY_NAME_LENGTH 10
+#define CONSUMER_THREAD_INTERVAL_IN_SECONDS 3
 
 K_THREAD_STACK_DEFINE(consumer_stack_area, CONSUMER_STACK_SIZE);
 struct k_thread consumer_thread_data;
 extern void consumer_thread_fn(void *, void *, void *);
+
+/* Reference to shared queue from producer */
+extern struct k_msgq msg_queue;
 
 K_THREAD_DEFINE(consumer_thread_id, CONSUMER_STACK_SIZE, consumer_thread_fn, NULL, NULL, NULL, 
     CONSUMER_PRIORITY, 0, 0);
@@ -18,8 +23,16 @@ void consumer_thread_fn(void *, void *, void *)
 {
     LOG_INF("Consumer thread started!");
     
+    char received_city[MAX_CITY_NAME_LENGTH + 1] = {0};
+    
     while(1)
     {
-        k_sleep(K_SECONDS(5));
+        int result = k_msgq_get(&msg_queue, received_city, K_SECONDS(10));
+        if (result == 0) {
+            LOG_INF("Received city from queue: %s", received_city);
+        } else {
+            LOG_INF("Failed to get message from queue, result: %d", result);
+        }
+        k_sleep(K_SECONDS(CONSUMER_THREAD_INTERVAL_IN_SECONDS));
     }
 }
--- applications/msg_queues/src/producer.c
+++ applications/msg_queues/src/producer.c
@@ -6,21 +6,33 @@ LOG_MODULE_REGISTER(producer, LOG_LEVEL_INF);
 
 #define FIRST_ELEMENT 0
 #define NUM_OF_CITIES 10 
-// #define MAX_CITY_NAME_LENGTH 20  
+#define MAX_CITY_NAME_LENGTH 10     // Noty Including null terminator
+#define MSG_QUEUE_SIZE ((NUM_OF_CITIES)*(MAX_CITY_NAME_LENGTH + 1)*(sizeof(char))) 
 
 /* Setting the thread for Producer work*/
 #define PRODUCER_THREAD_INTERVAL_IN_SECONDS 1
-#define PRODUCER_STACK_SIZE 500
+#define PRODUCER_STACK_SIZE 2048
 #define PRODUCER_PRIORITY 5
 K_THREAD_STACK_DEFINE(producer_stack_area, PRODUCER_STACK_SIZE);
 struct k_thread producer_thread_data;
 extern void producer_thread_fn(void *, void *, void *);
 
+/* Shared message queue */
+char msg_queue_buffer[MSG_QUEUE_SIZE] = {0};
+struct k_msgq msg_queue;
+
+static int init_queue_once(void)
+{
+    k_msgq_init(&msg_queue, msg_queue_buffer, MAX_CITY_NAME_LENGTH + 1, NUM_OF_CITIES);
+    return 0;
+}
+SYS_INIT(init_queue_once, PRE_KERNEL_2, 0);
+
 K_THREAD_DEFINE(producer_thread_id, PRODUCER_STACK_SIZE, producer_thread_fn, NULL, NULL, NULL, 
     PRODUCER_PRIORITY, 0, 0);
 
 /* Database of cities to select from */
-const char *city_database[] = {
+const char *city_database[]= {
     "Paris", "Berlin", "Tokyo", "Madrit", "Rome", "London", "New York", "Moscow", "Beijing", "Sydney"};
 
 /*-------------------------------------
@@ -30,31 +42,37 @@ static uint32_t generate_number(uint32_t min, uint32_t max);
 
 void producer_thread_fn(void *, void *, void *)
 {
-    uint32_t rnd = generate_number(FIRST_ELEMENT, NUM_OF_CITIES-1);
+    uint32_t rnd = generate_number(FIRST_ELEMENT, NUM_OF_CITIES - 1);
 
     /* Msg queue data*/
-    struct k_msgq msg_queue;
-
-    for(int i = 0; i < NUM_OF_CITIES; i++) {
-        LOG_INF("Size of City %d: %d", i, sizeof(city_database[0]));
-    }
-
-    k_msgq_init(&msg_queue, city_database, sizeof(city_database[0]), NUM_OF_CITIES);
+    char city_name[MAX_CITY_NAME_LENGTH + 1] = {0};   
     
     LOG_INF("Producer thread started!");
     
     while(1)
     {
-        // LOG_INF("Random number: %d", rnd);
+        LOG_INF("-------------------");
         LOG_INF("Selected city: %s", city_database[rnd]);
-        int result = k_msgq_put(&msg_queue, city_database[rnd], K_SECONDS(10));
+        memset(city_name, 0, sizeof(city_name));
+        memcpy(city_name, city_database[rnd], strlen(city_database[rnd]));
+        int result = k_msgq_put(&msg_queue, city_name, K_SECONDS(5));
+        LOG_INF("k_msgq_put result: %d", result);
+
+        int offset = 0;
+        for (int i = 0; i < NUM_OF_CITIES; i++) {
+            offset = i * (MAX_CITY_NAME_LENGTH + 1);
+            if( strlen(&msg_queue_buffer[offset]) > 0) {
+                LOG_INF("Cities in queue: %s", &msg_queue_buffer[offset]);
+            }
+        }
+
         if (result != 0) {
             LOG_ERR("Failed to put message in queue");
         }
         k_sleep(K_SECONDS(PRODUCER_THREAD_INTERVAL_IN_SECONDS));
 
         // Search for new number to select new city
-        rnd = generate_number(FIRST_ELEMENT, NUM_OF_CITIES);
+        rnd = generate_number(FIRST_ELEMENT, NUM_OF_CITIES - 1);
     }
 }
 
